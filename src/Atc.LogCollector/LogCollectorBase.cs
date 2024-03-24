5c1e4548ff0745c91a66054ae020d9b4b2a101d1
David Kallesen <david@lmdk.dk>
2024-03-24 02:01:51 +0100

    chore: Prettify sourceSystem

--- src/Atc.LogCollector/LogCollectorBase.cs
+++ src/Atc.LogCollector/LogCollectorBase.cs
@@ -1,8 +1,11 @@
 // ReSharper disable InconsistentNaming
+// ReSharper disable InvertIf
 namespace Atc.LogCollector;
 
 public abstract class LogCollectorBase
 {
+    private static readonly char[] SourceIdentifierTrimChars = ['-', '_'];
+
     protected static string GetSourceSystemFromSourceIdentifier(
         string sourceIdentifier,
         string formatToExcludeDatePart = "yyyyMMdd")
@@ -19,7 +22,8 @@ public abstract class LogCollectorBase
             if (DateTime.TryParseExact(potentialDate, formatToExcludeDatePart, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
             {
                 // If a date is found, return the string without this date part
-                return string.Concat(sourceIdentifier.AsSpan(0, i - formatToExcludeDatePart.Length), sourceIdentifier.AsSpan(i));
+                var sourceSystem = string.Concat(sourceIdentifier.AsSpan(0, i - formatToExcludeDatePart.Length), sourceIdentifier.AsSpan(i));
+                return sourceSystem.Trim(SourceIdentifierTrimChars);
             }
         }
 
